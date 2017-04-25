d9d907ecbb5803ef6ffe448a551bdb3426bd78f5
James Skinner <spiralx@gmail.com>
2017-04-25 07:38:46 +0100

    Changed from Webpack to Rollup for build

---
+++ rollup.config.production.js
@@ -0,0 +1,15 @@
+import r_typescript from 'rollup-plugin-typescript'
+import typescript from 'typescript'
+
+// --------------------------------------------------------------------
+
+export default {
+  entry: 'src/index.ts',
+  dest: 'dist/console.message.js',
+  format: 'umd',
+  moduleName: 'ConsoleMessage',
+  sourceMap: true,
+  plugins: [
+    r_typescript({ typescript })
+  ]
+}
